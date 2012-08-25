cf82b955e65fce29a48855cb3533506612ff6008
Johan Carlin <johan.carlin@gmail.com>
2012-08-25 23:04:24 +0100

    forgot one

---
+++ SecondTiming.m
@@ -0,0 +1,30 @@
+classdef SecondTiming < Timing
+    % Timing object for running events relative to Psychtoolbox clock
+    % (ie GetSecs).
+    methods
+        function t = SecondTiming(varargin)
+            t = varargs2structfields(varargin,t);
+            t.units = 's';
+        end
+
+        function tim = begin(self)
+        % start the clock
+        % tim = begin();
+            [tim, t.first] = deal(GetSecs);
+        end
+
+        function tim = check(self)
+        % return the current clock time
+        % tim = check()
+            self.previous = self.current;
+            [self.current,tim] = deal(GetSecs);
+        end
+
+        function waituntil(self,abstime)
+        % wait until an absolute clock time. 
+        % waituntil(abstime)
+            invoke(self.scanobj,'CheckPulseSynchronyForTime',...
+                (abstime-GetSecs)*1e3);
+        end
+    end
+end
