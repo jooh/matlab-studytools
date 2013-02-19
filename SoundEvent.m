c0fb8f97cbc2d05369aa33419b18bd38f1d6b90d
Johan Carlin <johan.carlin@gmail.com>
2013-02-19 18:02:19 +0000

    added buffering of SoundEvents

--- SoundEvent.m
+++ SoundEvent.m
@@ -1,20 +1,23 @@
 classdef SoundEvent < StudyEvent
     % Class for presenting audio stimuli with PsychPortAudio
     properties
-        audiodata % nchannels by nsamples
+        buffer % handle to PsychPortAudio createbuffer
         psychaudio
         name = 'sound'
+        timeouts = .5;
     end
     
     methods
         function s = SoundEvent(sound,st)
-            s.audiodata = sound;
             s.psychaudio = st.psychaudio;
+            s.buffer = PsychPortAudio('createbuffer',s.psychaudio,sound);
         end
         
         function call(self)
-            PsychPortAudio('FillBuffer',self.psychaudio,self.audiodata);
+            PsychPortAudio('FillBuffer',self.psychaudio,self.buffer);
             PsychPortAudio('Start',self.psychaudio);
+            % try to prevent distortions in sound playback
+            WaitSecs('YieldSecs',self.timeouts);
         end
     end
 end
