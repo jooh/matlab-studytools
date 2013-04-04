47f0fb8f154bf25da4d14db0199c4f7bda53f4d3
Johan Carlin <johan.carlin@gmail.com>
2013-04-04 12:08:39 +0100

    better emulation of ScannerSync behaviour

--- ScanObjNull.m
+++ ScanObjNull.m
@@ -3,7 +3,12 @@
 % CheckPulseSyncronyForTime (which waits but does nothing) and GetResponse
 % (which returns 0). This is only really useful for easily swapping out
 % ScannerSync specific code.
-classdef ScanObjNull
+classdef ScanObjNull < handle
+    properties
+        tr = [];
+        firstscantime = [];
+    end
+
     methods
         function s = ScanObjNull;
             %fprintf('WARNING: null scanobj - NO SCANNER SYNC\n')
@@ -13,10 +18,17 @@ classdef ScanObjNull
             switch varargin{1}
                 case 'CheckPulseSynchronyForTime'
                     WaitSecs(varargin{2}/1000);
-                case 'GetResponse'
+                case {'GetResponse','Initialize'}
                     varargout{1} = 0;
-                case 'Initialize'
+                case 'StartExperiment'
+                    self.firstscantime = GetSecs * 1000;
+                    self.tr = varargin{2};
                     varargout{1} = 0;
+                case 'GetMeasuredTR'
+                    varargout{1} = self.tr;
+                case 'GetLastPulseNum'
+                    varargout{1} = floor(...
+                        (GetSecs*1000-self.firstscantime)/ self.tr);
             end
         end
     end
