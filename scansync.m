9111fb6fed47ab953be95b972365d0fbcf243d12
Stim15 User <stim15user@mrc-cbsu.local>
2019-07-15 16:13:52 +0100

    bug fix

--- scansync.m
+++ scansync.m
@@ -62,7 +62,7 @@ if isempty(daqstate)
     hasdaq = false;
     try
         D = daq.getDevices;
-        hasdaq = D.isvalid && any(strcmp({D.Vendor.ID},'ni')) && ...
+        hasdaq = ~isempty(D) && D.isvalid && any(strcmp({D.Vendor.ID},'ni')) && ...
                 D.Vendor.isvalid && D.Vendor.IsOperational;
     catch err
         if ~strcmp(err.identifier,'MATLAB:undefinedVarOrClass')
